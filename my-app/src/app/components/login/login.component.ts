f157137f0b5b77d17cc945c13dba7535f25c38b4
gaby <gabriellycamilly01@gmail.com>
2023-04-06 22:55:29 -0300

    aula06/04

--- my-app/src/app/components/login/login.component.ts
+++ my-app/src/app/components/login/login.component.ts
@@ -8,7 +8,25 @@ import { Component } from '@angular/core';
 export class LoginComponent {
   isError = false;
 
+  isCorrect = false;
+
+  user = ""
+  pwd = ""
+
   login (){
-    this.isError = true;
+
+      if(this.user == "gaby" && this.pwd == "gaby") {
+        this.isCorrect= true 
+        this.isError=false
+      }else{
+
+        this.isError = true;
+        this.isCorrect=false
+      }
+
+
+    console.log("user = " + this.user + " Password ="  + this.pwd)
+
+
   }
 }
