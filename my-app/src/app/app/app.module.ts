2ac3cee28b6dc930ae2b6d427c228d1705738b45
gabyzinha21 <126827955+gabyzinha21@users.noreply.github.com>
2023-04-25 15:57:38 -0400

    Add files via upload

---
+++ my-app/src/app/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { GridComponent } from './components/grid/grid.component';
+import { PageNotFoudComponent } from './components/page-not-foud/page-not-foud.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroComponent,
+    LoginComponent,
+    GridComponent,
+    PageNotFoudComponent,
+    DashboardComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ my-app/src/app/app/components/page-not-foud/page-not-foud.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-foud',
+  templateUrl: './page-not-foud.component.html',
+  styleUrls: ['./page-not-foud.component.css']
+})
+export class PageNotFoudComponent {
+
+}
