decc9214f862342eda7c5fbca2145a251e6785cb
gabyzinha21 <126827955+gabyzinha21@users.noreply.github.com>
2023-06-20 16:08:49 -0400

    aula 20-06

--- my-app/src/app/components/task/task.component.ts
+++ my-app/src/app/components/task/task.component.ts
@@ -1,5 +1,6 @@
+import { Task } from './../../model/task';
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task',
@@ -8,16 +9,41 @@ import { FormControl } from '@angular/forms';
 })
 export class TaskComponent {
 
-  tasks = []
 
-  task = new FormControl('')
+
+  nome = new FormControl('')
   descricao = new FormControl ('')
   responsavel = new FormControl ('')
   dt_inicio = new FormControl('')
   dt_fim = new FormControl('')
 
+  Tasks: Task[] = []
+
   addtask() {
-    console.log(this.task)
+   let t = this.dataTObject();
+   this.Tasks.push(t);
+   console.log(t);
+  this.limparform();
   }
+
+  dataTObject(){
+    let task = new Task();
+    task.nome = this.nome.value!;
+    task.descricao = this.descricao.value!;
+    task.responsavel = this.responsavel.value!;
+    task.dt_inicio = Number(this.dt_inicio.value)!;
+    task.dt_fim = Number(this.dt_fim.value!);
+
+    return task;
+    
+  }
+
+limparform() {
+  this.nome.setValue('');
+  this.descricao.setValue('');
+  this.responsavel.setValue('');
+  this.dt_inicio.setValue('');
+  this.dt_fim.setValue('');
+}
 
 }
